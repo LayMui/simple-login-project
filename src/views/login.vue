f8b3ef080f15c41871e4564d5344718c4b902c26
LayMui <laymui.toh@taiger.com>
2020-12-27 20:34:41 +0800

    Understanding the concept

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,7 +3,7 @@
         <h1>Login</h1>
         <input type="text" name="username" v-model="input.username" placeholder="Username" />
         <input type="password" name="password" v-model="input.password" placeholder="Password" />
-        <button type="button" v-on:click="login()">Login</button>
+        <button type="button" @click="login()">Login</button>
     </div>
 </template>
 
@@ -22,13 +22,14 @@
             login() {
                 if(this.input.username != "" && this.input.password != "") {
                     if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
-                        this.$emit("authenticated", true);
+                       // this.$emit("authenticated", true);
+                        this.$parent.authenticated = true;
                         this.$router.replace({ name: "secure" });
                     } else {
-                        console.log("The username and / or password is incorrect");
+                        alert("The username and / or password is incorrect");
                     }
                 } else {
-                    console.log("A username and password must be present");
+                    alert("A username and password must be present");
                 }
             }
         }
